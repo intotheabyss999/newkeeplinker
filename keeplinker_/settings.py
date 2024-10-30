fb2bace76f81dbaf89e1b5e93e9383ec2a5206fa
Sergey Derkach <sergey@MacBook-2021-Pro-Sergey.local>
2024-10-30 10:01:55 +0200

    Fix project structure

--- keeplinker_/settings.py
+++ keeplinker_/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-2e2$tpdeqgk2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
