2b8467ad5b649411a84311d9543a72c650228411
Sergey Derkach <sergey@MacBook-2021-Pro-Sergey.local>
2024-11-01 02:01:32 +0200

    Your commit message here

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -2,9 +2,7 @@
 
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
-
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, PasswordResetForm, AuthenticationForm
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -13,6 +11,37 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add placeholders to signup form
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Username'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'you@email.com'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter your password'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Confirm your password'
+        })
+
+class CustomSignInForm(AuthenticationForm):
+    username = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'you@email.com',
+        'id': 'email'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your password',
+        'id': 'password'
+    }))
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     old_password = forms.CharField(widget=forms.PasswordInput(attrs={
@@ -27,7 +56,6 @@ class CustomPasswordChangeForm(PasswordChangeForm):
         'class': 'form-control', 
         'placeholder': 'Confirm New Password'
     }))
-
 
 class CustomPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(
